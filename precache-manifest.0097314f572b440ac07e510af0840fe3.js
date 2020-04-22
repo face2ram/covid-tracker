9da27f97b669ef4cb99a6a155ae059602af40c83
face2ram <bgsanap@gmail.com>
2020-04-22 19:14:15 +0530

    Updates

---
+++ precache-manifest.0097314f572b440ac07e510af0840fe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1ad9bd280447792e9b6c446c65264d",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "9578a98b101fe4f01db6",
+    "url": "/covid-tracker/static/css/main.f5e76c96.chunk.css"
+  },
+  {
+    "revision": "fb2991f7c30f795f00df",
+    "url": "/covid-tracker/static/js/2.8209a7b3.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-tracker/static/js/2.8209a7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9578a98b101fe4f01db6",
+    "url": "/covid-tracker/static/js/main.fd087312.chunk.js"
+  },
+  {
+    "revision": "5282512ee85b075753e0",
+    "url": "/covid-tracker/static/js/runtime-main.31c7ae0c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
